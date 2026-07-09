f2f3ea022b54959c7a8fdb638a47d8be88ed6342
august <kira_volkova_7@mail.ru>
2026-07-09 16:52:24 +0500

    all tasks

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование", help_text="Введите наименование продукта")
+    text = models.TextField(max_length=200, verbose_name="Описание", help_text="Введите описание продукта")
+    image = models.ImageField(
+        upload_to="products/image",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите изображение продукта",
+    )
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Укажите категорию продукта",
+        blank=True,
+        null=True,
+    )
+    price = models.FloatField(verbose_name="Цена", help_text="Укажите цену продукта")
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        ordering = ["name", "category", "price"]
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Категория", help_text="Введите наименование категории")
+    text = models.TextField(
+        max_length=300, blank=True, null=True, verbose_name="Описание", help_text="Введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
@@ -7,3 +8,17 @@ def home(request):
 
 def contacts(request):
     return render(request, "contacts.html")
+
+
+def home(request):
+    latest_products = Product.objects.order_by('-id')[:5]
+
+    print("--- Последние 5 созданных продуктов ---")
+    for product in latest_products:
+        print(f"ID: {product.id}, Название: {product.name}, Дата: {product.created_at}")
+    print("-------------------------------------")
+
+    context = {
+        'latest_products': latest_products
+    }
+    return render(request, "home.html", context)
