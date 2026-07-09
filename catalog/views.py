b1cdf7b6b8f537593a09afa65f8a71f467c8b887
august <kira_volkova_7@mail.ru>
2026-07-09 19:16:33 +0500

    add post

--- catalog/views.py
+++ catalog/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 from catalog.models import Product
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 
 
 def home(request):
     return render(request, "home.html")
 
 
+@csrf_protect
 def contacts(request):
-    return render(request, "contacts.html")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        return HttpResponse( f'Спасибо, {name}. Сообщение получено.')
+    else:
+        return render(request, "contacts.html")
 
 
 def home(request):
     latest_products = Product.objects.order_by('-id')[:5]
 
-    print("--- Последние 5 созданных продуктов ---")
     for product in latest_products:
         print(f"ID: {product.id}, Название: {product.name}, Дата: {product.created_at}")
-    print("-------------------------------------")
 
     context = {
         'latest_products': latest_products
